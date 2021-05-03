f61ff9e3c54c735e178783bbe976b7b1808fcd17
José Simões <jose.simoes@eclo.solutions>
2021-05-03 14:52:52 +0100

    Fix receiver thread declaration in several platforms

--- targets/FreeRTOS/_common/WireProtocol_ReceiverThread.c
+++ targets/FreeRTOS/_common/WireProtocol_ReceiverThread.c
@@ -7,24 +7,19 @@
 #include "task.h"
 #include "WireProtocol_HAL_Interface.h"
 
-extern WP_Message inboundMessage;
-
-void WP_Message_Initialize(WP_Message *a);
-void WP_Message_PrepareReception(WP_Message *a);
-void WP_Message_Process(WP_Message *a);
+void WP_Message_Process();
+void WP_Message_PrepareReception();
 
 void ReceiverThread(void *argument)
 {
     (void)argument;
 
+    WP_Message_PrepareReception();
+
     // loop forever
     while (1)
     {
-
-        WP_Message_Initialize(&inboundMessage);
-        WP_Message_PrepareReception(&inboundMessage);
-
-        WP_Message_Process(&inboundMessage);
+        WP_Message_Process();
 
         // Allow other tasks a chance to run
         taskYIELD();
--- targets/FreeRTOS_ESP32/ESP32_WROOM_32/common/WireProtocol_ReceiverThread.c
+++ targets/FreeRTOS_ESP32/ESP32_WROOM_32/common/WireProtocol_ReceiverThread.c
@@ -6,24 +6,19 @@
 #include <esp32_os.h>
 #include "WireProtocol_HAL_Interface.h"
 
-extern WP_Message inboundMessage;
-
-void WP_Message_Initialize(WP_Message *a);
-void WP_Message_PrepareReception(WP_Message *a);
-void WP_Message_Process(WP_Message *a);
+void WP_Message_Process();
+void WP_Message_PrepareReception();
 
 void ReceiverThread(void const *argument)
 {
     (void)argument;
 
+    WP_Message_PrepareReception();
+
     // loop forever
     while (1)
     {
-
-        WP_Message_Initialize(&inboundMessage);
-        WP_Message_PrepareReception(&inboundMessage);
-
-        WP_Message_Process(&inboundMessage);
+        WP_Message_Process();
 
         // Allow other tasks a chance to run
         vTaskDelay(0);
